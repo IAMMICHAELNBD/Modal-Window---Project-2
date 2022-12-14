deed29d1a86bf774a7373dd36e8ae96e370229bd
IAMMICHAELNBD <michaelgroettger@outlook.com>
2022-12-14 09:50:59 -0600

    Working with classes - adding functionality

--- script.js
+++ script.js
@@ -6,12 +6,19 @@ const btnCloseModal = document.querySelector('.close-modal');
 const btnsShowModal = document.querySelectorAll('.show-modal');
 console.log(btnsShowModal);
 
+const openModal = function () {
+  console.log('Button Clicked');
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
 // Selects Multiple Elements with one class.
 for (let i = 0; i < btnsShowModal.length; i++)
-  btnsShowModal[i].addEventListener('click', function () {
-    console.log('Button Clicked');
-    modal.classList.remove('hidden');
-    overlay.classList.remove('hidden');
-
-    modal.getElementsByClassName.display = 'block';
-  });
+  btnsShowModal[i].addEventListener('click', openModal);
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
