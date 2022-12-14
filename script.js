7557ec6b16bc1cf4b0b39449c95f231c70e78dbc
IAMMICHAELNBD <michaelgroettger@outlook.com>
2022-12-14 14:12:31 -0600

    Added escape key to close modal

--- script.js
+++ script.js
@@ -7,7 +7,6 @@ const btnsShowModal = document.querySelectorAll('.show-modal');
 console.log(btnsShowModal);
 
 const openModal = function () {
-  console.log('Button Clicked');
   modal.classList.remove('hidden');
   overlay.classList.remove('hidden');
 };
@@ -23,3 +22,11 @@ for (let i = 0; i < btnsShowModal.length; i++) {
 }
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (e) {
+  // console.log(e.key);
+
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
